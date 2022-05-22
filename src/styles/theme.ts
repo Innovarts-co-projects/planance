badc6fe8a42df667876520d295d4acb51ca6045e
stwgabriel <gabrielstw@pm.me>
2022-05-22 17:41:59 -0300

    chore: add app theme colors
    additional
    - turn eslint rule off on styled.d.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,21 @@
 const theme = {
   colors: {
-    text: '#333',
-    background: '#fefefe',
+    text: '#4B4A49',
+    background: '#FDFBF9',
+
+    green: '#1AB250',
+    red: '#FF1A1A',
+    purple: '#9747FF',
+
+    primary: {
+      main: '#FF7A00',
+    },
+
+    gray: {
+      main: '#D9D9D9',
+      light: '#F6F4F3',
+      lighter: '#FAFAFA',
+    },
   },
   effects: {},
   borders: {},
