59708357834039822ba759d1f6a215347cc129ed
stwgabriel <gabrielstw@pm.me>
2022-05-30 15:07:18 -0300

    chore: create sections
    - chart section
    - menus
    - start transactions section

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,12 @@
+import Image from 'next/image';
+
+import userPic from '../assets/icons/user-pic.svg';
+import entrance from '../assets/icons/entrance.svg';
+// import withdrawal from '../assets/icons/withdrawal.svg';
+// import balance from '../assets/icons/balance.svg';
+import plusIcon from '../assets/icons/plus.svg';
+import calendarIcon from '../assets/icons/calendar.svg';
+
 import MetaTitle from '../components/MetaTitle';
 
 import { DashboardContainer } from '../styles/pages/dashboard';
@@ -19,11 +28,116 @@ function Dashboard() {
           </div>
 
           <div className="profile-info">
+            <Image src={userPic} layout="raw" />
             <h2 className="user-name">
               Angela Smith
             </h2>
           </div>
         </header>
+
+        <section className="chart-section">
+          <div className="chart-group">
+            <div className="chart" />
+            <div className="chart-info">
+              <h3>Balance overview</h3>
+
+              <ul>
+                <li>
+                  <div className="chart-indentifier" />
+                  <span className="chart-category">
+                    Income
+                  </span>
+                </li>
+                <li>
+                  <div className="chart-indentifier" />
+                  <span className="chart-category">
+                    Expenses
+                  </span>
+                </li>
+                <li>
+                  <div className="chart-indentifier" />
+                  <span className="chart-category">
+                    Investments
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="balance-overview">
+            <div className="asset-display">
+              <header>
+                <div className="asset-icon">
+                  <Image src={entrance} layout="raw" />
+                </div>
+                <h3>Entrance</h3>
+              </header>
+              <main>
+                <h4 className="asset-value">
+                  R$ 3.500,00
+                </h4>
+              </main>
+            </div>
+
+            <div className="asset-display">
+              <header>
+                <div className="asset-icon">
+                  <Image src={entrance} layout="raw" />
+                </div>
+                <h3>Entrance</h3>
+              </header>
+              <main>
+                <h4 className="asset-value">
+                  R$ 3.500,00
+                </h4>
+              </main>
+            </div>
+
+            <div className="asset-display">
+              <header>
+                <div className="asset-icon">
+                  <Image src={entrance} layout="raw" />
+                </div>
+                <h3>Entrance</h3>
+              </header>
+              <main>
+                <h4 className="asset-value">
+                  R$ 3.500,00
+                </h4>
+              </main>
+            </div>
+          </div>
+        </section>
+
+        <section className="menus-section">
+          <button type="button" className="month-selector">
+            <Image src={calendarIcon} layout="raw" />
+            <span>July</span>
+          </button>
+          <button type="button" className="add-transaction">
+            <Image src={plusIcon} layout="raw" />
+            <span>Add transaction</span>
+          </button>
+        </section>
+
+        <section className="transactions-section">
+          <header>{' '}</header>
+
+          <ul>
+            <li>
+              {' '}
+            </li>
+            <li>
+              {' '}
+            </li>
+            <li>
+              {' '}
+            </li>
+            <li>
+              {' '}
+            </li>
+          </ul>
+        </section>
       </main>
     </DashboardContainer>
   );
--- src/styles/pages/dashboard.ts
+++ src/styles/pages/dashboard.ts
@@ -4,13 +4,11 @@ const DashboardContainer = styled.div`
   display: flex;
   width: 100vw;
 
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.02);
-
   display: flex;
 
   width: 100%;
 
-  padding: 0 14rem;
+  padding: 0 14rem 5rem;
 
   main.dashboard-main {
     display: flex;
@@ -34,11 +32,220 @@ const DashboardContainer = styled.div`
 
       h1.app-logo {
         color: ${({ theme }) => theme.color.primary.main};
-        font-size: 3.2rem;
+        font-size: 3.4rem;
+
+        margin-bottom: 1rem;
       }
 
       h2.page-title {
         font-size: 1.8rem;
+      }
+
+      .profile-info {
+        display: flex;
+
+        align-items: center;
+
+        img {
+          margin-right: 2rem;
+          width: 5rem;
+        }
+
+        h2 {
+          font-size: 2rem;
+        }
+      }
+    }
+
+    section.chart-section {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      width: 100%;
+
+      .chart-group {
+        display: flex;
+        align-items: center;
+
+        .chart {
+          background: #0005;
+          border-radius: 50%;
+
+          height: 17.5rem;
+          width: 17.5rem;
+
+          margin-right: 2rem;
+        }
+
+        .chart-info {
+          height: fit-content;
+          padding: 1rem;
+
+          h3 {
+            font-size: 1.6rem;
+            font-weight: 700;
+            margin-bottom: 2rem;
+          }
+        }
+
+        ul {
+          li {
+            list-style: none;
+
+            display: flex;
+            align-items: center;
+
+            margin-bottom: 0.4rem;
+
+            .chart-indentifier {
+              background: red;
+              border-radius: 0.2rem;
+              height: 1.1rem;
+              width: 1.4rem;
+
+              margin-right: 1rem;
+            }
+
+            span {
+              font-size: 1.2rem;
+              font-weight: 500;
+            }
+          }
+        }
+      }
+
+      .balance-overview {
+        display: flex;
+
+        .asset-display {
+          background: ${({ theme }) => theme.color.gray.light};
+          border: solid 0.2rem ${({ theme }) => theme.color.gray.medium};
+          border-radius: 1rem;
+
+          display: flex;
+          flex-direction: column;
+
+          height: 10rem;
+          width: 18rem;
+
+          padding: 1rem;
+          margin-right: 1rem;
+
+          header {
+            display: flex;
+            align-items: center;
+
+            img {
+              margin-right: 1rem;
+              height: auto;
+              width: 2rem;
+            }
+
+            h3 {
+              font-size: 1.8rem;
+              font-weight: 500;
+            }
+          }
+
+          main {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            height: 100%;
+            width: 100%;
+
+            h4 {
+              font-size: 2rem;
+              font-weight: 600;
+            }
+          }
+
+          &:last-child {
+            margin-right: 0;
+          }
+        }
+      }
+    }
+
+    section.menus-section {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      width: 100%;
+      padding: 4rem 0;
+
+      .month-selector {
+        background: ${({ theme }) => theme.color.gray.light};
+        border-radius: 0.5rem;
+
+        display: flex;
+        align-items: center;
+
+        padding: 1rem;
+
+        img {
+          margin-right: 1rem;
+        }
+
+        span {
+          font-size: 1.6rem;
+          font-weight: 500;
+        }
+      }
+      .add-transaction {
+        background: ${({ theme }) => theme.color.primary.main};
+        border-radius: 1rem;
+        color: ${({ theme }) => theme.color.invertedText};
+        font-size: 1.8rem;
+        font-weight: 500;
+
+        display: flex;
+        align-items: center;
+
+        padding: 1.1rem;
+
+        img {
+          margin-right: 1rem;
+        }
+      }
+    }
+
+    section.transactions-section {
+      width: 100%;
+
+      header {
+      }
+
+      ul {
+        width: 100%;
+
+        li {
+          background: ${({ theme }) => theme.color.gray.light};
+          border-radius: 1rem;
+
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+
+          height: 5rem;
+          width: 100%;
+
+          margin-bottom: 1rem;
+
+          .transaction-category {
+          }
+          .transaction-value {
+          }
+          .transaction-data {
+          }
+          .transaction-description {
+          }
+          .options {
+          }
+        }
       }
     }
   }
