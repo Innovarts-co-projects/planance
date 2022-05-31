9b3df68319e72dd7dac3bd88b04873f80d0bca6d
stwgabriel <stwgabriel@pm.me>
2022-05-31 14:58:34 -0300

    chore: create transaction component

---
+++ src/components/Transaction/index.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image';
+
+import calendarIcon from '../../assets/icons/calendar.svg';
+import moreOptionsIcon from '../../assets/icons/more-options.svg';
+
+import { TransactionContainer } from './transactionStyles';
+
+interface TransactionProps {
+  transactionIcon: string;
+  transactionType: string;
+  transactionValue: string;
+  transactionDate: string;
+  transactionDescription: string;
+}
+
+function Transaction(
+  {
+    transactionIcon,
+    transactionType,
+    transactionValue,
+    transactionDate,
+    transactionDescription,
+  }: TransactionProps,
+) {
+
+  return (
+    <TransactionContainer>
+      <div className={`transaction-type transaction-item type-${transactionType.toLowerCase()}`}>
+        <Image src={transactionIcon} layout="raw" />
+
+        <span>{transactionType}</span>
+      </div>
+
+      <div className="transaction-value transaction-item">
+        <span>{transactionValue}</span>
+      </div>
+
+      <div className="transaction-date transaction-item">
+        <Image src={calendarIcon} layout="raw" />
+        <span>{transactionDate}</span>
+      </div>
+
+      <div className="transaction-description transaction-item">
+        <span>{transactionDescription}</span>
+      </div>
+
+      <button type="button" className="transaction-more-options">
+        <Image src={moreOptionsIcon} layout="raw" />
+      </button>
+    </TransactionContainer>
+  );
+}
+
+export default Transaction;
---
+++ src/components/Transaction/transactionStyles.ts
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+const TransactionContainer = styled.li`
+  background: ${({ theme }) => theme.color.gray.light};
+  border-radius: 1rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 5rem;
+  width: 100%;
+
+  padding: 0 2rem;
+  margin-bottom: 1rem;
+
+  .transaction-item {
+    display: flex;
+    align-items: center;
+    min-width: 20rem;
+
+    font-size: 1.5rem;
+    font-weight: 500;
+  }
+  .transaction-type {
+    img {
+      margin-right: 1rem;
+    }
+
+    h3 {
+      font-weight: inherit;
+    }
+
+    &.type-income {
+      span {
+        color: ${({ theme }) => theme.color.green};
+      }
+    }
+    &.type-expense {
+      span {
+        color: ${({ theme }) => theme.color.red};
+      }
+    }
+    &.type-investment {
+      span {
+        color: ${({ theme }) => theme.color.purple};
+      }
+    }
+  }
+  .transaction-value {
+  }
+  .transaction-date {
+    img {
+      margin-right: 1rem;
+    }
+  }
+  .transaction-description {
+    flex: 1;
+  }
+  .transaction-more-options {
+    border-radius: 0.5rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding: 1rem 1rem;
+
+    transition: background 0.25s ease-out;
+
+    &:hover {
+      background: ${({ theme }) => theme.color.gray.main};
+    }
+  }
+`;
+
+export { TransactionContainer };
