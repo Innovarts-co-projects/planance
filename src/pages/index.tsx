db5535db299b5cc4c6a5753f1fab8bf6bfea1d54
stwgabriel <gabrielstw@pm.me>
2022-06-28 19:21:54 -0300

    chore: send access '/' to '/dashboard'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import MetaTitle from '../components/MetaTitle';
 
 import { HomeContainer } from '../styles/pages/home';
 
 function Home(): JSX.Element {
+
+  const route = useRouter();
+
+  useEffect(() => { route.push('/dashboard'); }, []);
+
   return (
     <HomeContainer>
       <MetaTitle page="Home" />
-      <h1>Home</h1>
+      <h1>Loading</h1>
     </HomeContainer>
   );
 }
